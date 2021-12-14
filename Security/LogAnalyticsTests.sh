b6d1de3ddb4c1abb4a20c411756fd791ed847345
Markus Lintuala <markus@lintuala.fi>
2021-12-14 21:26:25 +0200

    Fixed missing | and added KQL query sorting

--- Security/LogAnalyticsTests.sh
+++ Security/LogAnalyticsTests.sh
@@ -3,7 +3,7 @@
 counter=1
 while [ $counter -le 10 ]
 do
-logger -p local4.warn -t CEF "CEF:0|Markus|MLI|0.1|MarkusTests|MarkusTest-$counter|5"
+logger -p local4.warn -t CEF "CEF:0|Markus|MLI|0.1|MarkusTests|MarkusTest-$counter|5|"
 sleep 10
 ((counter++))
 done
@@ -12,3 +12,4 @@ done
 #Check CEF test messages from Common Security Log
 CommonSecurityLog
 | where DeviceProduct == "MLI"
+| sort by TimeGenerated desc 
